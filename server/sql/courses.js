cdcd8142e8c5339ee56da8f73ad8eb4b3e1854f4
shlomorosental <126162213+shlomorosental@users.noreply.github.com>
2024-03-14 17:32:26 +0200

    14.03(2)

--- server/sql/courses.js
+++ server/sql/courses.js
@@ -1,7 +1,7 @@
 const {pool} = require ("../db")
 
 async function getCourses() {
-    const sql = `SELECT courses.name, courses.class ,CONCAT(teachers.first_name, ' ' ,teachers.last_name) AS teacher
+    const sql = `SELECT courses.name ,CONCAT(teachers.first_name, ' ' ,teachers.last_name) AS teacher, courses.class
       FROM courses
       INNER JOIN teachers ON courses.teacher_id = teachers.id;`;
     const res = await pool.query(sql);
@@ -9,5 +9,5 @@ async function getCourses() {
   }
 
 
-  
+
 module.exports = getCourses;
